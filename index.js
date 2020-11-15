a6020d45d592a65e9279e920cc723bce94c48f12
ktsulun <ktsulun@gmail.com>
2020-11-15 13:36:29 +0200

    new commit with button attribute

---
+++ index.js
@@ -0,0 +1,34 @@
+const startBtn = document.querySelector('button[data-action="start"]');
+const stopBtn = document.querySelector('button[data-action="stop"]');
+const body = document.body;
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+let intervalId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startBtn.addEventListener(`click`, startColorSwitch);
+
+function startColorSwitch() {
+  intervalId = setInterval(() => {
+    startBtn.disabled = true;
+    body.style.background =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+  }, 1000);
+}
+
+stopBtn.addEventListener(`click`, stopColorSwitch);
+
+function stopColorSwitch() {
+  startBtn.disabled = false;
+  clearInterval(intervalId);
+}
